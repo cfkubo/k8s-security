40fbc1bfa09160cc037c6cf950cb1c56f5885507
Arul Khanna Vannala <35385340+cfkubo@users.noreply.github.com>
2025-02-02 15:17:16 -0800

    Update multipass-k8s-setup.sh

--- multipass-k8s-setup.sh
+++ multipass-k8s-setup.sh
@@ -2,6 +2,6 @@ brew install multipass
 
 multipass launch --name kubemaster --cpus 2 --memory 2GB --disk 20GB
 
-multipass launch --name kubeworker01 --cpus 2 --mem 2GB --disk 20GB
-multipass launch --name kubeworker02 --cpus 2 --mem 2GB --disk 20GB
+multipass launch --name kubeworker01 --cpus 2 --memory 2GB --disk 20GB
+multipass launch --name kubeworker02 --cpus 2  --memory 2GB --disk 20GB
 
