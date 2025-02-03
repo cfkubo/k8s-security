5e9a93a49af3f183c385b5dadfa2acbd1185f2a4
Arul Khanna Vannala <35385340+cfkubo@users.noreply.github.com>
2025-02-02 16:53:02 -0800

    Update multipass-k8s-setup.sh

--- multipass-k8s-setup.sh
+++ multipass-k8s-setup.sh
@@ -5,3 +5,4 @@ multipass launch --name kubemaster --cpus 2 --memory 2GB --disk 20GB
 multipass launch --name kubeworker01 --cpus 2 --memory 2GB --disk 20GB
 multipass launch --name kubeworker02 --cpus 2  --memory 2GB --disk 20GB
 
+# multipass transfer <vm-name>:/path/to/file/on/vm /local/path/to/save/file. 
