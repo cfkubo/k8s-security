207170e6549c57fd24799ba3eb4372dc15b2c81e
Arul Khanna Vannala <35385340+cfkubo@users.noreply.github.com>
2025-02-02 17:40:04 -0800

    Update upgrade-k8s.sh

--- upgrade-k8s.sh
+++ upgrade-k8s.sh
@@ -1,4 +1,4 @@
- echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.30/deb/ /' | sudo tee /etc/apt/sources.list.d/kubernetes.list
+echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.30/deb/ /' | sudo tee /etc/apt/sources.list.d/kubernetes.list
 sudo kubeadm upgrade plan
 sudo apt update
 sudo apt-cache madison kubeadm
