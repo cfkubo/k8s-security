1bf4d5eedcfd0ed7bf4a6489fb6129b007f947b5
Arul Khanna Vannala <35385340+cfkubo@users.noreply.github.com>
2025-02-06 18:09:53 -0800

    Update kube-linter.sh

--- kube-linter.sh
+++ kube-linter.sh
@@ -1,8 +1,9 @@
 # kube-linter install
 sudo apt install make
 sudo snap install go --classic
+go install golang.stackrox.io/kube-linter/cmd/kube-linter@latest
 
-git clone https://github.com/stackrox/kube-linter
-cd kube-linter
-make build
-.gobin/kube-linter version
+# git clone https://github.com/stackrox/kube-linter
+# cd kube-linter
+# make build
+# .gobin/kube-linter version
