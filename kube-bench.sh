0fa3e155a757d6183d459732ed29b17a05b6d56d
Arul Khanna Vannala <35385340+cfkubo@users.noreply.github.com>
2025-02-07 02:30:09 -0800

    Update kube-bench.sh

--- kube-bench.sh
+++ kube-bench.sh
@@ -4,7 +4,7 @@ git clone https://github.com/aquasecurity/kube-bench
 cd kube-bench
 
 make build
-sudo cp bin/kube-bench /usr/local/bin/
+sudo cp kube-bench /usr/local/bin/
 
 sudo chown root:root kube-bench
 sudo chmod +x kube-bench
