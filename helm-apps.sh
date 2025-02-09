428fc3365fbf3c871941bda8e6686e88198ef043
cfkubo <arul.vannala@gmail.com>
2025-02-09 06:24:24 -0700

    update helm apps

--- helm-apps.sh
+++ helm-apps.sh
@@ -27,6 +27,8 @@ helm upgrade --install cnpg \
 helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
 helm repo update
 helm install prometheus prometheus-community/kube-prometheus-stack --namespace monitoring --create-namespace
+## add promethus source to grafana : http://prometheus-operated.monitoring.svc.cluster.local:9090
+
 
 ## ArgoCD
 kubectl create namespace argocd
