f55183af2de6695aca56ea618805ec84dfb9e8c3
cfkubo <arul.vannala@gmail.com>
2025-02-09 05:50:45 -0700

    updates to helm apps

--- helm-apps.sh
+++ helm-apps.sh
@@ -34,3 +34,44 @@ helm upgrade --install cnpg \
   --namespace cnpg-system \
   --create-namespace \
   cnpg/cloudnative-pg
+
+## Prometheus Grafana Loki
+helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
+helm repo update
+helm install prometheus prometheus-community/kube-prometheus-stack --namespace monitoring --create-namespace
+
+## ArgoCD
+kubectl create namespace argocd
+helm repo add argo https://argoproj.github.io/argo-helm
+helm install argocd argo/argo-cd --namespace argocd --create-namespace
+
+# ## KubeDB
+# helm repo add appscode https://charts.appscode.com/stable/
+# helm install kubedb-operator appscode/kubedb --namespace kubedb --create-namespace
+
+# ## KubeDB Postgres
+# helm install postgres kubedb/postgres --namespace kubedb --create-namespace
+
+# ## KubeDB Elasticsearch
+# helm install elasticsearch kubedb/elasticsearch --namespace kubedb --create-namespace
+
+# ## KubeDB Minio
+# helm install minio kubedb/minio --namespace kubedb --create-namespace
+
+# ## KubeDB Redis
+# helm install redis kubedb/redis --namespace kubedb --create-namespace
+
+# ## KubeDB MongoDB
+# helm install mongodb kubedb/mongodb --namespace kubedb --create-namespace
+
+# ## KubeDB MySQL 
+# helm install mysql kubedb/mysql --namespace kubedb --create-namespace
+
+# ## KubeDB MariaDB
+# helm install mariadb kubedb/mariadb --namespace kubedb --create-namespace
+
+# ## KubeDB Memcached
+# helm install memcached kubedb/memcached --namespace kubedb --create-namespace
+
+# ## KubeDB ProxySQL
+# helm install proxysql kubedb/proxysql --namespace kubedb --create-namespace
